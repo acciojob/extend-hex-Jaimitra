ffdd76ae4088fc349dd61e0e8335b95d46af4722
Jaimitra <141642600+Jaimitra@users.noreply.github.com>
2023-11-14 20:51:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let a = '#';
+	for(i=1;i<shortHex.length;i++)
+		{
+			a =a+ shortHex[i] + shortHex[i];
+		}
+	return a;
 };
 
 // Do not change the code below.
